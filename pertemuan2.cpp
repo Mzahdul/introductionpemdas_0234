59b8f3af2079433731ad30fea7a0d48b5345ef73
Mzahdul <seeglas8@gmail.com>
2026-02-23 21:13:22 +0700

    menghitung persegi

---
+++ pertemuan2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){ //mulai
+    int p, l, luas;
+
+    //langkah 2
+    cout << "masukan panjang : ";
+    cin >> p;
+    
+    //langkah 3 
+    cout << "masukan lebar : ";
+    cin >> l;   
+
+    //langkah 4
+    luas = p * l;
+}
